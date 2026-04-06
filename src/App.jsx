119779bcd65b01fe0a667eaa546030fa1ace07d2
BajiLinux <mr.baji.smart@gmail.com>
2026-04-06 17:32:10 +0700

    Beresin bagian App dulu geys

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
 // ? App.jsx
-import React from 'react';
+import { Component } from 'react';
+import { getAllNotes } from './utils/local-data';
 
-function App() {
-  return (
-    <div className="app-container">
-      <h1>Hello, React</h1>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      notes: getAllNotes()
+    }
+  }
+  
+  render() {
+    const {notes} = this.state;
+    return(
+      <section className="app-container">
+        <header>
+          <h1>Baji Notes App</h1>
+        </header>
+        <main>
+          <ul>
+            {notes.map(note => (
+              <>
+                <br />
+                <li>{note.body}</li>
+                <br />
+              </>
+            ))}
+          </ul>
+        </main>
+      </section>
+    );
+  };
 }
-
-export default App;
